cae36393dd480d1d5e1040437da7a203836a5077
unknown <keleralexei@gmail.com>
2021-11-22 00:08:25 +0300

    <Add> Factory method version 2. Need to change to fit Factory pattern.

---
+++ Factory_ver2/Factory/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    public class Point
+    {
+        public double a { get; set; }
+        public double b { get; set; }
+
+        public Point(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+    }
+}
