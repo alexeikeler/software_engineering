a6a14ceb4896cc63c74b8e7e8965dda19bf1d5ca
unknown <keleralexei@gmail.com>
2021-11-23 09:40:25 +0300

    <Add> Non conflict version for vs2019.

---
+++ FactoryMethod_sdk5.0/FactoryMethod/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    //Product class
+    public abstract class Product
+    {
+        public abstract string Operation(string function);
+    }
+}
---
+++ FactoryMethod_sdk5.0/FactoryMethod/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FactoryMethod
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> samples = new List<string>();
+
+            Console.Write("Enter samples: ");
+            foreach(string str in Console.ReadLine().Split(" "))
+            {
+                samples.Add(str);
+            }
+
+            foreach(string sample in samples)
+            {
+                Console.WriteLine("----------------------------------------------------------------");
+                Console.WriteLine($"Test # 1\n" +
+                              "| INTEGRATION PROCEDURE |\n" +
+                              $"| f = {sample} |\n" +
+                              $"| F = {new IntegrationCreator().Calculate(sample)} |\n");
+
+                Console.WriteLine($"Test # 2\n" +
+                                  "| Differentiation PROCEDURE |\n" +
+                                  $"| f = {sample} |\n" +
+                                  $"| f' = {new DifferentiationCreator().Calculate(sample)}|\n");
+
+                Console.WriteLine($"Test # 3\n" +
+                                  "| LIMIT PROCEDURE |\n" +
+                                  $"| f = {sample} |\n" +
+                                  $"| Lim as x -> +oo = {new LimitCreator().Calculate(sample)} |\n");
+
+            }
+
+
+
+        }
+    }
+}
